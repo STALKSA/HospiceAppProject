9db8b305a257c9c6053d9eef3baee90dffed9e14
ksenia <ksy184694@gmail.com>
2024-03-24 15:31:32 +0300

    adding Login, main, navigation pages

---
+++ app/src/androidTest/java/ru/iteco/fmhandroid/page/MainPage.java
@@ -0,0 +1,35 @@
+package ru.iteco.fmhandroid.page;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static ru.iteco.fmhandroid.test.TestUtils.waitDisplayed;
+import androidx.test.espresso.ViewInteraction;
+import ru.iteco.fmhandroid.R;
+public class MainPage {
+    ViewInteraction newsTitle = onView(withId(R.id.container_list_news_include_on_fragment_main));
+    ViewInteraction missionButton = onView(withId(R.id.our_mission_image_button));
+
+    ViewInteraction allNewsButton = onView(withId(R.id.all_news_text_view));
+
+    public void validatePageLoaded() {
+        newsTitle.check(matches(isDisplayed()));
+        missionButton.check(matches(isDisplayed()));
+        allNewsButton.check(matches(isDisplayed()));
+    }
+
+    public void waitUntilPageLoaded() {
+        onView(isRoot()).perform(waitDisplayed(R.id.container_list_news_include_on_fragment_main, 10000));
+    }
+
+    public void goToMissionPage() {
+        missionButton.perform(click());
+    }
+    
+    public void goToAllNewsButton() {
+        allNewsButton.perform(click());
+    }
+}
---
+++ app/src/androidTest/java/ru/iteco/fmhandroid/page/NavPage.java
@@ -0,0 +1,51 @@
+package ru.iteco.fmhandroid.page;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import androidx.test.espresso.ViewInteraction;
+import junit.framework.AssertionFailedError;
+import ru.iteco.fmhandroid.R;
+
+public class NavPage {
+    ViewInteraction toastButton = onView(withId(R.id.main_menu_image_button));
+    ViewInteraction newsPageButton = onView(withText("News"));
+    ViewInteraction mainPageButton = onView(withText("Main"));
+    ViewInteraction aboutPageButton = onView(withText("About"));
+
+    public void goToNewsPage() {
+        toastButton.check(matches(isDisplayed()));
+        toastButton.perform(click());
+
+        newsPageButton.check(matches(isDisplayed()));
+        newsPageButton.perform(click());
+    }
+
+    public void goToAboutPage() {
+        toastButton.check(matches(isDisplayed()));
+        toastButton.perform(click());
+
+        aboutPageButton.check(matches(isDisplayed()));
+        aboutPageButton.perform(click());
+    }
+
+    public void goToMainPage() {
+        toastButton.check(matches(isDisplayed()));
+        toastButton.perform(click());
+
+        mainPageButton.check(matches(isDisplayed()));
+        mainPageButton.perform(click());
+    }
+
+    public boolean checkIfToastButtonVisible(){
+        try {
+            toastButton.check(matches(isDisplayed()));
+            return true;
+        } catch (AssertionFailedError e) {
+            return false;
+        }
+    }
+}
