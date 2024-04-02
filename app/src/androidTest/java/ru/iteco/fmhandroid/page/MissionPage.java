e1abf1be56e042fd9633807e283e524b288ebb3b
ksenia <ksy184694@gmail.com>
2024-04-02 19:30:41 +0300

    adding MissionPage and AboutTest

---
+++ app/src/androidTest/java/ru/iteco/fmhandroid/page/MissionPage.java
@@ -0,0 +1,24 @@
+package ru.iteco.fmhandroid.page;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static ru.iteco.fmhandroid.test.TestUtils.waitDisplayed;
+import androidx.test.espresso.ViewInteraction;
+import ru.iteco.fmhandroid.R;
+
+public class MissionPage {
+    ViewInteraction missionItemList = onView(withId(R.id.our_mission_item_list_recycler_view));
+
+
+    public void validatePageLoaded() {
+        missionItemList.check(matches(isDisplayed()));
+    }
+
+    public void waitUntilPageLoaded() {
+        onView(isRoot()).perform(waitDisplayed(R.id.our_mission_item_list_recycler_view, 6000));
+    }
+
+}
---
+++ app/src/androidTest/java/ru/iteco/fmhandroid/test/AboutTest.java
@@ -0,0 +1,56 @@
+package ru.iteco.fmhandroid.test;
+
+import static ru.iteco.fmhandroid.test.AuthUtils.goToMainPage;
+import androidx.test.espresso.Espresso;
+import org.junit.Before;
+import org.junit.Test;
+import io.qameta.allure.kotlin.Allure;
+import io.qameta.allure.kotlin.Description;
+import io.qameta.allure.kotlin.Epic;
+import io.qameta.allure.kotlin.Feature;
+import io.qameta.allure.kotlin.Story;
+import ru.iteco.fmhandroid.BasicTest;
+import ru.iteco.fmhandroid.page.AboutPage;
+import ru.iteco.fmhandroid.page.NavPage;
+
+@Epic("Внутренние страницы приложения")
+@Feature("О приложении")
+public class AboutTest extends BasicTest {
+
+    @Before
+    public void login() {
+        goToMainPage();
+    }
+    @Test
+    @Story("Переход на экран О Приложении")
+    @Description("Переход на экран О Приложении с экрана Main")
+    public void goToAboutPageFromMainTest() {
+        Allure.step("Вызвать меню");
+        NavPage navPage = new NavPage();
+        Allure.step("Перейти на страницу О приложении");
+        navPage.goToAboutPage();
+
+        AboutPage aboutPage = new AboutPage();
+        aboutPage.waitUntilPageLoaded();
+        aboutPage.validatePageLoaded();
+
+        Espresso.pressBack();
+    }
+    @Test
+    @Story("Переход на экран О Приложении")
+    @Description("Переход на экран О Приложении с экрана News")
+    public void goToAboutPageFromNewsTest() {
+        Allure.step("Перейти на экран News");
+        NavPage navPage = new NavPage();
+        navPage.goToNewsPage();
+
+        Allure.step("Перейти на страницу О приложении");
+        navPage.goToAboutPage();
+
+        AboutPage aboutPage = new AboutPage();
+        aboutPage.waitUntilPageLoaded();
+        aboutPage.validatePageLoaded();
+
+        Espresso.pressBack();
+    }
+}
